17d6e64c229298465c41fda1da740b9242edb4ac
Engineerbabu777 <awaismumtaz0099@gmail.com>
2025-02-21 16:22:10 +0500

    Add:/ bottom tabs navigation added

--- StackNavigator.tsx
+++ StackNavigator.tsx
@@ -7,19 +7,92 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import HomeScreen from './screens/HomeScreen'
 import LoginScreen from './screens/LoginScreen'
 import SignUpScreen from './screens/SignUpScreen'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { AntDesign, Entypo, Ionicons } from '@expo/vector-icons'
 
 
 const Stack = createNativeStackNavigator()
+const Tab = createBottomTabNavigator();
+function BottomTabs() {
+    return (
+        <Tab.Navigator>
+          <Tab.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{
+              tabBarLabel: "Home",
+              tabBarLabelStyle: { color: "black" },
+              headerShown: false,
+              tabBarIcon: ({ focused }) =>
+                focused ? (
+                  <Entypo name="home" size={24} color="black" />
+                ) : (
+                  <AntDesign name="home" size={24} color="black" />
+                ),
+            }}
+          />
+  
+          <Tab.Screen
+            name="Thread"
+            component={ThreadsScreen}
+            options={{
+              tabBarLabel: "Create",
+              tabBarLabelStyle: { color: "black" },
+              headerShown: false,
+              tabBarIcon: ({ focused }) =>
+                focused ? (
+                  <Ionicons name="create" size={24} color="black" />
+                ) : (
+                  <Ionicons name="create-outline" size={24} color="black" />
+                ),
+            }}
+          />
+  
+          <Tab.Screen
+            name="Activity"
+            component={ActivityScreen}
+            options={{
+              tabBarLabel: "Activity",
+              tabBarLabelStyle: { color: "black" },
+              headerShown: false,
+              tabBarIcon: ({ focused }) =>
+                focused ? (
+                  <AntDesign name="heart" size={24} color="black" />
+                ) : (
+                  <AntDesign name="hearto" size={24} color="black" />
+                ),
+            }}
+          />
+  
+          <Tab.Screen
+            name="Profile"
+            component={ProfileScreen}
+            options={{
+              tabBarLabel: "Profile",
+              tabBarLabelStyle: { color: "black" },
+              headerShown: false,
+              tabBarIcon: ({ focused }) =>
+                focused ? (
+                  <Ionicons name="person" size={24} color="black" />
+                ) : (
+                  <Ionicons name="person-outline" size={24} color="black" />
+                ),
+            }}
+          />
+        </Tab.Navigator>
+      );
+}
+
 const StackNavigator = () => {
     return (
         <Stack.Navigator
-        screenOptions={{
-            headerShown: false,
-        }}
+            screenOptions={{
+                headerShown: false,
+            }}
         >
             <Stack.Screen name="login" component={LoginScreen} />
             <Stack.Screen name="signup" component={SignUpScreen} />
-            <Stack.Screen name="Main" component={HomeScreen} />
+            <Stack.Screen name="Main" component={BottomTabs} />
 
         </Stack.Navigator>
     )
